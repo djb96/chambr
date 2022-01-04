507bda5a01ffda785c5aa1dba7ddb1ab16bdc54c
Jack Davison <45171616+jack-davison@users.noreply.github.com>
2022-01-04 17:27:23 +0000

    Init Commit

---
+++ R/plot_chamber.R
@@ -0,0 +1,27 @@
+
+#' Visualise Chamber Data
+#'
+#' To ensure that the chamber data has been read in correctly, this function
+#' uses ggplot2 to visualise all of the numeric variables against time. There
+#' is also an option to drop values that remained constant throughout.
+#'
+#' @param .data WACL chamber data read using read_chamber.
+#' @param drop_constant Should constant variables be plotted? (T/F, defaults to F)
+#'
+#' @return
+#' @export
+#'
+
+plot_chamber = function(.data, drop_constant = F){
+
+  if(drop_constant){
+    .data = janitor::remove_constant(dat = .data, quiet = T)
+  }
+
+  .data %>%
+    tidyr::pivot_longer(where(is.numeric)) %>%
+    ggplot2::ggplot(ggplot2::aes(x = time, y = value)) +
+    ggplot2::geom_line() +
+    ggplot2::facet_wrap(~name, scales = "free_y")
+
+}
---
+++ R/read_chamber.R
@@ -0,0 +1,47 @@
+
+#' Read Data from the WACL Chamber
+#'
+#' Reads data from the WACL chamber into R, fixing column names and data
+#' types. There will likely be parsing warnings - this is to be expected
+#' and nothing to worry about. It is due to the files being flanked by
+#' incomplete data with fewer columns.
+#'
+#' @param file The path to the file.
+#'
+#' @return
+#' @export
+#'
+
+read_chamber = function(file){
+
+  # Find the line at which the data actually starts
+  start = dplyr::tibble(text = readr::read_lines(file)) |>
+    dplyr::mutate(n = dplyr::row_number()) |>
+    dplyr::filter(stringr::str_detect(text, "MODE")) |>
+    dplyr::pull(n) |>
+    min()
+
+  # Read in the data, providing column names
+  df = readr::read_csv(file, skip = start-1,
+                       show_col_types = FALSE,
+                       col_names = c("time", "state", "alarms", "temp_c",
+                                     "emi_fa", "trap_v", "pump_percent",
+                                     "charger_i_uA", "charger_u_v", "bat_mv",
+                                     "conc_fa", "conc_ugm3")) |>
+
+    # time and mode are read in incorrectly - fix this here
+    tidyr::separate(time, into = c("time", "mode"), sep = 20) |>
+
+    # fix data types
+    dplyr::mutate(time = lubridate::dmy_hms(time),
+                  mode = stringr::str_remove(mode, "MODE"),
+                  state = stringr::str_remove(state, "STATE"),
+                  dplyr::across(alarms:conc_ugm3, readr::parse_number),
+                  dplyr::across(where(is.character), stringr::str_trim)) |>
+
+    # this step should get rid of the tail of the data
+    tidyr::drop_na(time)
+
+  return(df)
+
+}
